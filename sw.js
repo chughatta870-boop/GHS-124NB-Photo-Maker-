bc913122b326399c54a84857f0292570d9f4035b
chughatta870-boop <chughatta870@gmail.com>
2026-07-01 11:13:16 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "name": "Passport Photo Maker GHS 124NB",
+  "short_name": "Passport Maker",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4D96FF",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
